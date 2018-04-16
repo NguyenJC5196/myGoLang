ae5c88e0ff54fa7c6fb99137f5459d7d99445b14
Nguyen Ngoc Nhu Thuy <thuy.nguyen@groovetechnology.com>
2018-04-16 17:39:33 +0700

    apply pointer receriver in method

--- Structs.go
+++ Structs.go
@@ -10,11 +10,20 @@ type car struct {
 	steering_wheel int16 // (-32k, +32k) 
 	top_speed_kmh float64
 }
-func (c car) KmPerHour() float64{
+func (c *car) KmPerHour() float64{
+	c.top_speed_kmh = 500
 	return float64(c.gas_pedal) * (c.top_speed_kmh/Use16Bitmax)
 }
-func (c car) MilePerHour() float64{
+func (c *car) MilePerHour() float64{
+	c.top_speed_kmh = 500
 	return float64(c.gas_pedal) * (c.top_speed_kmh/kmh_multiple)
+}
+func (c *car) New_top_speed(newSpeed float64){
+	c.top_speed_kmh = newSpeed
+}
+func newer_top_speed(c car, speed float64) car{
+	c.top_speed_kmh = speed
+	return c
 }
 func main(){
 	a_car := car{
@@ -24,6 +33,11 @@ func main(){
 		top_speed_kmh: 99999.0,
 	}
 	fmt.Println(a_car.gas_pedal)
+	fmt.Println(a_car.KmPerHour())
+	fmt.Println(a_car.MilePerHour())
+
+	// a_car.New_top_speed(500)
+	a_car = newer_top_speed(a_car, 500)
 	fmt.Println(a_car.KmPerHour())
 	fmt.Println(a_car.MilePerHour())
 }
