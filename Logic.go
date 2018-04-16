6332e5120609eec3a1cbc0af073fb145e1b19c5d
Nguyen Ngoc Nhu Thuy <thuy.nguyen@groovetechnology.com>
2018-04-16 16:52:30 +0700

    if-esle syntax

--- Logic.go
+++ Logic.go
@@ -3,6 +3,25 @@ package main
 import "fmt"
 
 func main(){
+	if 7 % 2 == 0{
+		fmt.Println("number is even")
+	} else{
+		fmt.Println("number is odd")
+	}
+
+	if 8 %4 == 0 {
+		fmt.Println("can be divided")
+	}
+
+	if num := 9; num < 0{
+		fmt.Println(num, "is negative")
+	} else if num < 10 {
+		fmt.Println(num, "has 1 digit")
+	} else {
+		fmt.Println(num, "bigger than 10")
+	}
+}
+func loop(){
 	i := 1
 	for i <= 3 {
 		fmt.Println(i)
